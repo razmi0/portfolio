ac128c0f7a7e0aedd26b90d5242a9776cfdf1a7a
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-06-05 00:05:01 +0200

    ⏰ checkpoint : Wed Jun  5 00:05:01 CEST 2024

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,37 @@ import HeadingTransition from "./components/HeadingTransition";
 import Hero from "./components/Hero";
 import { ModeToggle } from "./components/ModeToggle";
 import Presentation from "./components/Presentation";
-import { skills } from "./components/Skills/data.json" assert { type: "json" };
+import { projects, skills } from "./components/Skills/data.json" assert { type: "json" };
+import Icon from "./components/ui/icons/Icon";
 import Slider from "./components/ui/tabs/Slider";
 import Tabs from "./components/ui/tabs/Tabs";
 
-// const groupByType = <T extends { type: ("front-end" | "back-end")[] }>(data: T): { [key: T[type]]: T } => {
-//   const grouped = Object.entries(data);
-//   console.log(grouped);
-//   return grouped;
-// };
+const groupedByType = <T extends { type: string[] }>(data: T[]) => {
+  const grouped = {} as Record<string, T[]>;
+  data.forEach((item) => {
+    console.log(item);
+    item.type.forEach((type) => {
+      if (!grouped[type]) {
+        grouped[type] = [];
+      }
+      grouped[type].push(item);
+    });
+  });
+  return grouped;
+};
+
+const skillsByType = groupedByType(skills);
+const projectsByType = groupedByType(projects);
+
+const CardType = ({ title, level }: { title: string; level: string }) => {
+  return (
+    <div className="flex flex-col w-fit items-start justify-center card bg-bogoss-300/80 py-3 px-3 mx-1 [&>h4]:text-bogoss-200 gap-2 grow">
+      <Icon name={title.toLowerCase()} size={25} />
+      <h4 className="text-left w-full">{title}</h4>
+      <p className="text-sm">{level}</p>
+    </div>
+  );
+};
 
 const App = () => {
   // const [activeSkills, setActiveSkills] = useState(new Array(skills.length).fill({ active: false }));
@@ -46,11 +68,11 @@ const App = () => {
       {/* MES COMPETENCES */}
       {/* MES COMPETENCES */}
       {/* MES COMPETENCES */}
-      <HeadingTransition h2="Mes compétences" small="skills" />
+      <HeadingTransition h2="Mes compétences" small="skills" className="my-20" />
       <div>
         <Tabs.Root>
           <Tabs.Nav className="h-[10vh] gap-7 py-0 mb-5 justify-evenly">
-            <Slider className={"bg-bogoss-600"} />
+            <Slider className={"bg-bogoss-600 rounded-md"} />
             <Tabs.Trigger value="front-end" className="group h-full active:ring-bogoss-700 active:scale-110">
               <h2 className="text-xl group-data-[selected=true]:text-bogoss-400 ">Front-end</h2>
             </Tabs.Trigger>
@@ -62,17 +84,18 @@ const App = () => {
             </Tabs.Trigger>
           </Tabs.Nav>
           <Tabs.Content value="front-end" className="max-w-screen h-full">
-            {skills.map((content, i) => {
-              return (
-                <>
-                  {content.title}
-                  {i}
-                </>
-              );
-            })}
+            <div className="flex">
+              {skillsByType["front-end"].map((content) => {
+                return <CardType key={content.id} title={content.title} level={content.level} />;
+              })}
+            </div>
           </Tabs.Content>
           <Tabs.Content value="back-end" className="max-w-screen">
-            <div className="h-screen ring-1 ring-white"></div>
+            <div className="flex">
+              {skillsByType["back-end"].map((content) => {
+                return <CardType key={content.id} title={content.title} level={content.level} />;
+              })}
+            </div>
           </Tabs.Content>
           <Tabs.Content value="soft" className="max-w-screen">
             Soft
@@ -122,12 +145,12 @@ const App = () => {
       {/* MES EXPERIENCES */}
       {/* MES EXPERIENCES */}
       {/* MES EXPERIENCES */}
-      <HeadingTransition h2="Mes expériences" small="mon parcours" />
+      <HeadingTransition h2="Mes expériences" small="mon parcours" className="my-20" />
 
       <div>
         <Tabs.Root>
           <Tabs.Nav className="h-[10vh] gap-7 bg-slate-900 py-0 mb-5 justify-evenly">
-            <Slider className={"bg-slate-800"} />
+            <Slider className={"bg-slate-800 rounded-md"} />
             <Tabs.Trigger value="tous" className="group h-full active:ring-slate-800 active:scale-110">
               <h2 className="text-xl group-data-[selected=true]:text-slate-500 ">Tous</h2>
             </Tabs.Trigger>
@@ -143,11 +166,11 @@ const App = () => {
           <Tabs.Content value="formation">formation</Tabs.Content>
         </Tabs.Root>
 
-        <HeadingTransition h2="Mes projets" small="portfolio" />
+        <HeadingTransition h2="Mes projets" small="portfolio" className="my-20" />
 
         <Tabs.Root>
           <Tabs.Nav className="h-[10vh] gap-7 bg-slate-900 py-0 mb-5 justify-evenly">
-            <Slider className={"bg-slate-800"} />
+            <Slider className={"bg-slate-800 rounded-md"} />
             <Tabs.Trigger value="tous" className="group h-full active:ring-slate-800 active:scale-110">
               <h2 className="text-xl group-data-[selected=true]:text-slate-500 ">Tous</h2>
             </Tabs.Trigger>
--- src/components/HeadingTransition.tsx
+++ src/components/HeadingTransition.tsx
@@ -1,6 +1,8 @@
-const HeadingTransition = ({ h2, small }: { h2: string; small: string }) => {
+import { cn } from "@/lib/utils";
+
+const HeadingTransition = ({ h2, small, className }: { h2: string; small: string; className?: string }) => {
   return (
-    <div className="h-[10vh] w-full text-center my-0 space-y-2">
+    <div className={cn("h-[10vh] w-full text-center my-0 space-y-2", className)}>
       <small className="dark:text-bogoss-200 text-bogoss-700 font-semibold">{small}</small>
       <h2 className="text-5xl uppercase">{h2}</h2>
     </div>
