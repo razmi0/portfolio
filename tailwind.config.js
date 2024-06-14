6d24ea28a9921a8906a25445d72636af35cd87e7
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-06-14 16:36:42 +0200

    Remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,22 +26,6 @@ module.exports = {
           500: "#ff6700",
         },
       },
-
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 };
