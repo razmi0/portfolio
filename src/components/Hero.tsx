5dd6ea3d9594523913108c7c1fe457acf57bda5f
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-06-15 16:30:14 +0200

    Fix styling issues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div {...rest} className="flex flex-col items-center justify-evenly h-[95vh] w-full">
       <div className="text-center [&>h2]:text-bogoss-700 [&>h2]:dark:text-bogoss-200  ">
-        <h2 className="text-lg ">Hello there, </h2>
+        <h2 className="text-lg ">Hello there,</h2>
         <h1 className="text-3xl !text-belgoss-500">I'm Thomas</h1>
         <h2 className="text-lg mb-4">I'm web developer</h2>
         <div className="flex flex-wrap items-center justify-center gap-4">
@@ -20,11 +20,11 @@ const Hero = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
         </div>
       </div>
       <div className="relative w-full grid place-content-center">
-        <div className="absolute -left-8 sm:left-0 bottom-0 space-y-3 text-bogoss-400">
+        <div className="absolute left-0 bottom-0 text-belgoss-500">
           <a
             href="https://www.linkedin.com/in/dev-web-cuesta-thomas/"
             aria-label="link to Thomas linkedin to get in touch with him">
-            <LinkedinIcon />
+            <LinkedinIcon className="mb-3" />
           </a>
           <a
             href="https://github.com/razmi0"
@@ -37,7 +37,7 @@ const Hero = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
           className="h-[250px] sm:h-[300px] brightness-95 contrast-125 aspect-square object-cover"
           alt="hero picture of Thomas portfolio looking at the right side and smiling"
         />
-        <VerticalText text="Présentation" className="-right-14 sm:right-0" />
+        <VerticalText text="Présentation" className="hidden sm:block" />
       </div>
     </div>
   );
