981c2539d1384a030b005f17ed67f71924e18ac8
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-06-20 13:12:32 +0200

    Add default lazy load to Carousel Image and eager on Modal

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,10 +3,11 @@ import { HTMLAttributes } from "react";
 interface ImageProps extends HTMLAttributes<HTMLImageElement> {
   src: string;
   alt: string;
+  loading?: "eager" | "lazy";
 }
 
-const Image = ({ ...props }: ImageProps) => {
-  return <img {...props} decoding="async" loading="lazy" />;
+const Image = ({ loading = "lazy", ...props }: ImageProps) => {
+  return <img {...props} decoding="async" loading={loading} />;
 };
 
 export default Image;
