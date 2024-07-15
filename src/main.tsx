5815f3f179076f4b9ed25ef767580bc571a300a1
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-15 22:31:45 +0200

    Add AuthProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./tailwind/output.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { AuthProvider } from "./hooks/useAuth.tsx";
+import "./tailwind/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
