7b5bebf8f41c00a78d834638849ffd9585e93e9c
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-15 22:31:36 +0200

    Update with user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import Experience from "@/components/Cards/Experience";
 import Project from "@/components/Cards/Project";
+import { IceCreamBowlIcon } from "lucide-react";
 import { useState, type HTMLAttributes } from "react";
 import Carousel from "./components/Carousel";
 import Contact from "./components/Contact";
 import HeadingTransition from "./components/HeadingTransition";
 import Hero from "./components/Hero";
+import Login from "./components/Login/Login";
 import { ModeToggle } from "./components/ModeToggle";
 import Presentation from "./components/Presentation";
 import { RisingStars } from "./components/RisingStars/RisingStars";
 import Skills from "./components/Skills/Skills";
 import { useTheme } from "./components/theme-provider";
 import { NavButton } from "./components/ui/button";
+import Show from "./components/ui/show";
 import { formation, projects, skills, xp } from "./data.json";
+import useAgent from "./hooks/useAgent";
 import useFilters from "./hooks/useFilter";
+import useRouter from "./hooks/useRouter";
 import useTitle from "./hooks/useTitle";
 import { cn, uppercase } from "./lib/utils";
-import useAgent from "./hooks/useAgent";
 
 const initStates = {
   skills: Array(skills.data.length)
@@ -28,122 +32,138 @@ const initStates = {
 
 const App = () => {
   const [skillsHovered, setSkillsHovered] = useState<boolean[]>(initStates.skills);
+
   const { filters, handleFilterChange, values } = useFilters();
   const { titles } = useTitle();
   const { theme } = useTheme();
   useAgent();
 
+  const { changeRoute, current } = useRouter();
+
   return (
-    <main className="relative min-w-full h-full flex flex-col" style={{ viewTransitionName: "none" }}>
-      <header className="flex items-center justify-between flex-row-reverse absolute top-0 left-0 w-full mt-5">
+    <main className="relative min-w-full min-h-screen flex flex-col" style={{ viewTransitionName: "none" }}>
+      <header className="flex items-center justify-start flex-row-reverse absolute top-0 left-0 w-full mt-5 gap-3">
+        <button type="button" onClick={current !== "login" ? () => changeRoute("login") : () => changeRoute("index")}>
+          <IceCreamBowlIcon className={"h-7 w-7"} />
+        </button>
         <ModeToggle />
       </header>
+      <Show when={current === "login"}>
+        <Login />
+      </Show>
       <RisingStars />
-      <Hero id={titles.hero.selector} />
+      <Show when={current === "index"}>
+        <Hero id={titles.hero.selector} />
 
-      <HeadingTransition
-        h2="A propos de moi"
-        small="présentation"
-        className="mb-20"
-        id={titles.presentation.selector}
-      />
-      <Presentation />
-      {/* SKILLS */}
-      {/* SKILLS */}
-      {/* SKILLS */}
-      {/* SKILLS */}
-      <HeadingTransition
-        h2="Mes compétences"
-        small="skills"
-        className="my-20 mt-44 [&_h2]:text-4xl sm:[&_h2]:text-5xl"
-        id={titles.skills.selector}></HeadingTransition>
-      <Skills.Root>
-        <Skills.Grid setter={setSkillsHovered} skills={skills.data} />
-        <Skills.Article skills={skills.data} skillHovered={skillsHovered} />
-      </Skills.Root>
-      <RisingStars />
+        <HeadingTransition
+          h2="A propos de moi"
+          small="présentation"
+          className="mb-20"
+          id={titles.presentation.selector}
+        />
+        <Presentation />
+        {/* SKILLS */}
+        {/* SKILLS */}
+        {/* SKILLS */}
+        {/* SKILLS */}
+        <HeadingTransition
+          h2="Mes compétences"
+          small="skills"
+          className="my-20 mt-44 [&_h2]:text-4xl sm:[&_h2]:text-5xl"
+          id={titles.skills.selector}></HeadingTransition>
+        <Skills.Root>
+          <Skills.Grid setter={setSkillsHovered} skills={skills.data} />
+          <Skills.Article skills={skills.data} skillHovered={skillsHovered} />
+        </Skills.Root>
+        <RisingStars />
 
-      {/* Experience */}
-      {/* Experience */}
-      {/* Experience */}
-      {/* Experience */}
+        {/* Experience */}
+        {/* Experience */}
+        {/* Experience */}
+        {/* Experience */}
 
-      <HeadingTransition h2="Mes expériences" small="mon parcours" className="my-20" id={titles.xp.selector} />
-      <Flex>
-        <NavSection>
-          {values.xp.map((value) => {
-            const handler = () => handleFilterChange("xp", value);
-            return (
-              <NavButton match={filters.xp === value} key={value} onClick={handler}>
-                {uppercase(value)}
-              </NavButton>
-            );
-          })}
-        </NavSection>
-        <Experience.Root>
-          {[...xp, ...formation].map((content, i) => {
-            const is = `${content.type === "pro" ? "pro" : "formation"}-${content.id}` as const;
-            const folder = theme === "dark" ? `dark/` : "";
-            return (
-              <Experience.Card
-                key={content.id}
-                className={filters.xp === "tous" || content.type.includes(filters.xp) ? "" : "hidden"}
-                src={`cards-bg/${folder}${i + 1}.webp`}
-                is={is}>
-                <Experience.Content content={content} />
-              </Experience.Card>
-            );
-          })}
-        </Experience.Root>
-      </Flex>
-      <RisingStars />
-
-      {/* PROJECT */}
-      {/* PROJECT */}
-      {/* PROJECT */}
-      {/* PROJECT */}
-      {/* PROJECT */}
-
-      <HeadingTransition h2="Mes projets" small="portfolio" className="mb-20 mt-44" id={titles.projects.selector} />
-
-      <Flex>
-        <NavSection>
-          {values.projects.map((value) => {
-            const handler = () => handleFilterChange("projects", value);
-            return (
-              <NavButton match={filters.projects === value} key={value} onClick={handler}>
-                {uppercase(value)}
-              </NavButton>
-            );
-          })}
-        </NavSection>
-        <section className="flex place-content-center gap-2 mt-10 w-full">
-          <Project.Grid>
-            {projects.map((content, i) => {
-              const hidden = filters.projects === "tous" || content.type.includes(filters.projects) ? "" : "hidden";
+        <HeadingTransition h2="Mes expériences" small="mon parcours" className="my-20" id={titles.xp.selector} />
+        <Flex>
+          <NavSection>
+            {values.xp.map((value) => {
+              const handler = () => handleFilterChange("xp", value);
               return (
-                <Project.Card key={content.id} content={content} className={hidden + " p-0"}>
-                  <Carousel content={content} key={i} />
-                </Project.Card>
+                <NavButton match={filters.xp === value} key={value} onClick={handler}>
+                  {uppercase(value)}
+                </NavButton>
               );
             })}
-          </Project.Grid>
-        </section>
-      </Flex>
-      <RisingStars />
+          </NavSection>
+          <Experience.Root>
+            {[...xp, ...formation].map((content, i) => {
+              const is = `${content.type === "pro" ? "pro" : "formation"}-${content.id}` as const;
+              const folder = theme === "dark" ? `dark/` : "";
+              return (
+                <Experience.Card
+                  key={content.id}
+                  className={filters.xp === "tous" || content.type.includes(filters.xp) ? "" : "hidden"}
+                  src={`cards-bg/${folder}${i + 1}.webp`}
+                  is={is}>
+                  <Experience.Content content={content} />
+                </Experience.Card>
+              );
+            })}
+          </Experience.Root>
+        </Flex>
+        <RisingStars />
 
-      {/* CONTACT */}
-      {/* CONTACT */}
-      {/* CONTACT */}
-      {/* CONTACT */}
-      {/* CONTACT */}
+        {/* PROJECT */}
+        {/* PROJECT */}
+        {/* PROJECT */}
+        {/* PROJECT */}
+        {/* PROJECT */}
 
-      <HeadingTransition h2="Contactez-moi" small="me joindre" className="my-44 h-[5vh]" id={titles.contact.selector} />
+        <HeadingTransition h2="Mes projets" small="portfolio" className="mb-20 mt-44" id={titles.projects.selector} />
 
-      <Flex className="mb-44">
-        <Contact />
-      </Flex>
-      <RisingStars />
+        <Flex>
+          <NavSection>
+            {values.projects.map((value) => {
+              const handler = () => handleFilterChange("projects", value);
+              return (
+                <NavButton match={filters.projects === value} key={value} onClick={handler}>
+                  {uppercase(value)}
+                </NavButton>
+              );
+            })}
+          </NavSection>
+          <section className="flex place-content-center gap-2 mt-10 w-full">
+            <Project.Grid>
+              {projects.map((content, i) => {
+                const hidden = filters.projects === "tous" || content.type.includes(filters.projects) ? "" : "hidden";
+                return (
+                  <Project.Card key={content.id} content={content} className={hidden + " p-0"}>
+                    <Carousel content={content} key={i} />
+                  </Project.Card>
+                );
+              })}
+            </Project.Grid>
+          </section>
+        </Flex>
+        <RisingStars />
+
+        {/* CONTACT */}
+        {/* CONTACT */}
+        {/* CONTACT */}
+        {/* CONTACT */}
+        {/* CONTACT */}
+
+        <HeadingTransition
+          h2="Contactez-moi"
+          small="me joindre"
+          className="my-44 h-[5vh]"
+          id={titles.contact.selector}
+        />
+
+        <Flex className="mb-44">
+          <Contact />
+        </Flex>
+        <RisingStars />
+      </Show>
     </main>
   );
 };
