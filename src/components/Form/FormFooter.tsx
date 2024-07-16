477e8bce6a7a70c16a63113bde1af4b9c6e352fb
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-16 22:32:40 +0200

    Refactor FormFooter component to display success and failure messages

--- src/components/Form/FormFooter.tsx
+++ src/components/Form/FormFooter.tsx
@@ -1,16 +1,31 @@
-import { Check } from "lucide-react";
-import SubmitButton from "./SubmitButton";
 import type { FormStatusType } from "@/hooks/useForm";
+import { Check } from "lucide-react";
+import Show from "../ui/show";
+import SubmitButton from "./SubmitButton";
 
-const FormFooter = ({ formStatus, successText }: { formStatus: FormStatusType; successText: string }) => {
+type FormFooterType = {
+  formStatus: FormStatusType;
+  successText: string;
+  failText: string;
+};
+
+const FormFooter = ({ formStatus, successText, failText }: FormFooterType) => {
+  const Status = () => {
+    const color = formStatus === "success" ? "green" : "red";
+    const text = formStatus === "success" ? successText : failText;
+    return (
+      <Show when={formStatus}>
+        <p className={`${color} gap-3 inline-flex justify-center slide-from-bottom slide-from-active`}>
+          <Check />
+          {text}
+        </p>
+      </Show>
+    );
+  };
+
   return (
     <div className="flex justify-end items-center input-group gap-5">
-      {formStatus === "success" && (
-        <p className="text-green-500 gap-3 inline-flex justify-center slide-from-bottom slide-from-active">
-          <Check />
-          {successText}
-        </p>
-      )}
+      <Status />
       <SubmitButton formStatus={formStatus} loadingText="Envoi en cours..">
         Envoyer
       </SubmitButton>
