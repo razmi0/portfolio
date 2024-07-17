055a6eb2ae0b03ac7a073b4c491c7fdacb268691
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-17 11:33:59 +0200

    Add console logs to useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -9,7 +9,10 @@ export const useAuth = (): AuthData => {
   }
 
   useEffect(() => {
+    console.log("useEffect from useAuth : ");
     const payload = localStorage.getItem("auth");
+    console.log("payload from useAuth : ", payload);
+
     if (payload) {
       context?.signIn(() => Promise.resolve(JSON.parse(payload)));
     }
