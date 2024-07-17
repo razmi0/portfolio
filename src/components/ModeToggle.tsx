ed3cf4fe305c732253e2da9e8bf5111d855735f5
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-18 00:02:16 +0200

    Add Tooltip component to ModeToggle and create PreviousButton component

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,6 +1,7 @@
 import { Moon, Sun } from "lucide-react";
 
 import { useTheme } from "@/components/theme-provider";
+import Tooltip from "./ui/tooltip";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
@@ -16,10 +17,12 @@ export function ModeToggle() {
   };
 
   return (
-    <button onClick={rotateTheme} className="p-2 dark:bg-bogoss-500 bg-bogoss-200 grid place-content-center">
-      <Sun className="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </button>
+    <Tooltip tooltip={"change theme"} className="-translate-x-[40%]">
+      <button onClick={rotateTheme} className="p-2 dark:bg-bogoss-500 bg-bogoss-200 grid place-content-center">
+        <Sun className="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span className="sr-only">Toggle theme</span>
+      </button>
+    </Tooltip>
   );
 }
---
+++ src/components/PreviousButton.tsx
@@ -0,0 +1,16 @@
+import type { Routes } from "@/types";
+import { ArrowLeft } from "lucide-react";
+import Show from "./ui/show";
+import Tooltip from "./ui/tooltip";
+
+const PreviousButton = ({ previous, onClick }: { previous: Routes | null; onClick: () => void }) => (
+  <Show when={previous}>
+    <Tooltip tooltip={<>Previous page</>} className="-translate-x-[37%]">
+      <button onClick={onClick}>
+        <ArrowLeft className="h-7 w-7" />
+      </button>
+    </Tooltip>
+  </Show>
+);
+
+export default PreviousButton;
