4f25ccd6036175f058e4b0a7150e57ceae320a83
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-18 21:26:27 +0200

    Refactor Tab component in tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -12,18 +12,20 @@ type TabProps = {
 };
 const Tab = ({ label, open, onClick, children }: TabProps) => {
   return (
-    <section className="flex items-center justify-center gap-2" data-is="tab">
-      <Button
-        className={cn(
-          "flex gap-2 place-items-center dark:!text-white",
-          !open && "scale-90 bg-transparent dark:!text-white/50"
-        )}
-        onClick={onClick}>
-        {label}
-        <ChevronUp className={cn("size-6 transition-transform", open && "rotate-180")} />
-      </Button>
+    <>
+      <section className="flex items-center justify-center gap-2" data-is="tab">
+        <Button
+          className={cn(
+            "flex gap-2 place-items-center dark:!text-white",
+            !open && "scale-90 bg-transparent dark:!text-white/50"
+          )}
+          onClick={onClick}>
+          {label}
+          <ChevronUp className={cn("size-6 transition-transform", open && "rotate-180")} />
+        </Button>
+      </section>
       <Show when={open}>{children}</Show>
-    </section>
+    </>
   );
 };
 
