b985850374bc1c991790232fa3030f0403b5368a
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-19 15:47:46 +0200

    Fix tooltip positioning and opacity

--- src/components/Nav/ModeToggle.tsx
+++ src/components/Nav/ModeToggle.tsx
@@ -21,7 +21,7 @@ export function ModeToggle() {
   const [handleWithTransition] = useMemo(() => withViewTransition(handler), [theme]);
 
   return (
-    <Tooltip tooltip={"change theme"} className="-translate-x-[40%]">
+    <Tooltip tooltip={"change theme"} className="">
       <button onClick={handleWithTransition} className="p-2 dark:bg-bogoss-500 bg-bogoss-200 grid place-content-center">
         <Sun className="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -8,8 +8,11 @@ type TooltipProps = {
   disabled?: boolean;
   className?: string;
 };
+
 const Tooltip = ({ children, tooltip, className, disabled = false }: TooltipProps) => {
   const [btnDisabled, setBtnDisabled] = useState(disabled);
+  const [posX, setPosX] = useState<`translateX(-${number}px)`>();
+  const [firstRender, setFirstRender] = useState(true);
   const tooltipRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -19,11 +22,21 @@ const Tooltip = ({ children, tooltip, className, disabled = false }: TooltipProp
       return;
     }
     const parent = tooltipRef.current.parentElement;
+    const btn = parent?.firstElementChild as HTMLButtonElement;
     if (!parent) return;
-    const btn = parent.firstElementChild as HTMLButtonElement;
-    if (!btn) return;
+
+    if (firstRender) {
+      const translationX =
+        Math.floor(tooltipRef.current.getBoundingClientRect().width / 2) -
+        Math.floor(btn.getBoundingClientRect().width / 2);
+      setPosX(`translateX(-${translationX}px)`);
+    }
+
     setBtnDisabled(btn.disabled);
+    setFirstRender(false);
   }, [disabled]);
+
+  console.log(posX);
 
   return (
     <div className="relative flex group appearance-none size-fit select-none">
@@ -32,8 +45,9 @@ const Tooltip = ({ children, tooltip, className, disabled = false }: TooltipProp
         <div
           ref={tooltipRef}
           data-is="tooltip"
+          style={{ transform: posX }}
           className={cn(
-            "tooltip absolute -bottom-10 whitespace-nowrap hidden sm:group-hover:block pointer-events-none px-2 py-1 font-medium text-sm rounded-sm",
+            "tooltip absolute -bottom-10 whitespace-nowrap opacity-0 sm:group-hover:opacity-100 pointer-events-none px-2 py-1 font-medium text-sm rounded-sm",
             "bg-bogoss-600/60 text-white/90 dark:bg-bogoss-500/60",
             className
           )}>
