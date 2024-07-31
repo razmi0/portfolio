b93e7480119321499597067a370405cf49aaec66
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-31 14:22:35 +0200

    Remove React.StrictMode from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./provider/auth-provider.tsx";
@@ -7,13 +6,11 @@ import { ThemeProvider } from "./provider/theme-provider.tsx";
 import "./tailwind/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthProvider>
-        <RouterProvider>
-          <App />
-        </RouterProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <AuthProvider>
+      <RouterProvider>
+        <App />
+      </RouterProvider>
+    </AuthProvider>
+  </ThemeProvider>
 );
