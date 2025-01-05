026679cc5786c49ab4b41413c4d302a07f430ef8
razmiO <thomas.cuesta31@gmail.com>
2025-01-05 17:58:27 +0100

    styles (blogs)

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,4 +1,5 @@
 import { withViewTransition } from "@/lib/utils";
+import { ArrowLeft } from "lucide-react";
 import { useMemo, useState, type ReactNode } from "react";
 import Markdown from "react-markdown";
 import { blogs } from "../../data/blog.json";
@@ -11,11 +12,13 @@ type Blog = {
     content: string;
 };
 
-const BlogCard = ({ children, onClick, is }: { children: ReactNode; onClick: () => void; is: string }) => {
+const BlogCard = ({ children, onClick, is, ...rest }: { children: ReactNode; onClick: () => void; is: string }) => {
     const [handleWithTransition] = useMemo(() => withViewTransition(onClick), [onClick]);
     return (
-        <CardWrapper is={is} className="w-fit inline-flex !flex-row gap-2 max-w-[400px]">
-            <h3 className="block text-1xl !text-bogoss-300 w-fit">{children}</h3>
+        <CardWrapper {...rest} is={is} className="w-fit inline-flex !flex-row gap-2 max-w-[400px]">
+            <h3 className="block text-1xl text-black dark:text-white w-fit" style={{ viewTransitionName: "heading" }}>
+                {children}
+            </h3>
             <Button
                 onClick={handleWithTransition}
                 className="whitespace-nowrap"
@@ -29,8 +32,8 @@ const BlogCard = ({ children, onClick, is }: { children: ReactNode; onClick: () 
 
 const Article = ({ content }: { content: string }) => {
     return (
-        <div className="flex flex-col gap-4 px-5 [&_h1]:text-bogoss-300 [&_h1]:text-2xl [&_a]:text-belgoss-500 sm:[&_p]:px-5">
-            <Markdown>{content}</Markdown>
+        <div className="flex flex-col gap-4 px-5 [&_h1]:text-bogoss-300 [&_h1]:text-2xl [&_a]:text-belgoss-500 sm:[&_p]:px-5 max-w-5xl">
+            <Markdown className={"mb-12"}>{content}</Markdown>
         </div>
     );
 };
@@ -43,33 +46,30 @@ export default function Blog() {
     };
 
     return (
-        <div className="mt-44 flex flex-col items-start justify-start h-[95vh] w-full">
-            <div className="inline-flex justify-between flex-wrap">
-                {selectedBlog && (
-                    <Button
+        <div className="mt-44 flex flex-col items-center justify-start w-full gap-5">
+            {selectedBlog && (
+                <div className="w-full ps-2">
+                    <button
                         onClick={() => setSelectedBlog(null)}
-                        className="mb-10 h-fit"
-                        variant="outline"
-                        ariaLabel="go back to the list of blogs">
-                        Back to the list
-                    </Button>
-                )}
-                {selectedBlog ? (
-                    <h3 className="sm:max-w-[50%] mb-5 text-center text-black dark:text-white">
-                        {blogs.find((blog) => blog.id === selectedBlog)?.title || ""}
-                    </h3>
-                ) : (
-                    <h1 className="text-5xl !text-belgoss-500 mb-20">Blogs</h1>
-                )}
-            </div>
-            <div className="flex flex-col gap-3">
+                        className=""
+                        aria-label="go back to the list of blogs">
+                        <ArrowLeft className="size-5 text-bogoss-300 " />
+                    </button>
+                </div>
+            )}
+            {selectedBlog ? (
+                <h3 className="sm:max-w-[50%] text-3xl mb-5 text-center text-black dark:text-white">
+                    {blogs.find((blog) => blog.id === selectedBlog)?.title || ""}
+                </h3>
+            ) : (
+                <h1 className="text-5xl !text-belgoss-500 mb-20">Blogs</h1>
+            )}
+            <div className="flex flex-wrap gap-3">
                 {!selectedBlog &&
                     blogs.map((blog) => (
-                        <div key={blog.id}>
-                            <BlogCard is={blog.id.toString()} onClick={() => onSelect(blog.id)}>
-                                {blog.title}
-                            </BlogCard>
-                        </div>
+                        <BlogCard key={blog.id} is={blog.id.toString()} onClick={() => onSelect(blog.id)}>
+                            {blog.title}
+                        </BlogCard>
                     ))}
                 {selectedBlog && <Article content={blogs.find((blog) => blog.id === selectedBlog)?.content || ""} />}
             </div>
